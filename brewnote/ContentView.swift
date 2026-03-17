759c22cc56acd695088da46eec9cd475b63a9975
jdpenuliar <jaydz.penuliar@gmail.com>
2026-03-17 16:11:22 -0700

    base_app

---
+++ brewnote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  brewnote
+//
+//  Created by JD Penuliar on 3/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Brewnote")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
